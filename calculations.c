0830ef2d80c451d69c3c93fe9653c1ead8cd0ad3
kaiobarb <kaiobarb@gmail.com>
2019-02-14 03:08:05 -0800

    physics complete

--- calculations.c
+++ calculations.c
@@ -1,17 +1,15 @@
+#include <Python.h>
 #include <stdio.h>
 #include <math.h>
-#include <Python.h>
 
 #define GRAVITY_CONST 6.67428 * pow(10.0, -11.0)
 #define C 299.792458
-
-void init_calculations(void);
 
 main(int argc, char **argv) {
     // Initialize the python interpreter
     Py_Initialize();
     // Initialize our module
-    init_calculations();
+    //init_calculations();
     // Exit the interpreter
     Py_Exit(0);
 }
@@ -19,13 +17,36 @@ main(int argc, char **argv) {
 // Calculate Force
 // F = (G * M_a * M_b) / distance(a, b)^2
 static PyObject *calculateForce(PyObject *self, PyObject* args) {
-    
+    double force, distance;
+    double dx, dy, ma, ra, mb, rb;
+
+    if (!PyArg_ParseTuple(args, "dddddd", &dx, &dy, &ma, &ra, &mb, &rb))
+        return NULL;
+
+    // dx = bx - ax;
+    // dy = by - ay;
+    distance = sqrt(dx * dx + dy * dy);
+    if(distance < (ra + rb)) distance = ra + rb; 
+
+    ma = ma * 10000000000;
+	mb = mb * 10000000000;
+    distance = distance * 1000000 ;
+
+    force = (GRAVITY_CONST * ma * mb) / distance;
+
+    return PyFloat_FromDouble( force );
 }
 
 // Calculate distance between two bodies a and b
 // D = sqrt( (a_x - b_x)^2 + (a_y - b_y)^2 )
 static PyObject *calculateDist(PyObject *self, PyObject* args) {
+    double dx, dy, ra, rb, distance;
+    if (!PyArg_ParseTuple(args, "dddd", &dx, &dy, &ra, &rb))
+        return NULL;
+    distance = sqrt(dx * dx + dy * dy);
+    if(distance < (ra + rb)) distance = ra + rb; 
 
+    return PyFloat_FromDouble(distance);
 }
 
 // Calculate relativistic mass
@@ -38,18 +59,27 @@ static PyObject *calculateDist(PyObject *self, PyObject* args) {
 // RM = m_a / sqrt( 1 - ( ( sqrt(speed_x^2 + speed_y^2) )^2 ) / ( C^2 ) ); 
 // where C = 299.792458 (speed of light)
 static PyObject *calculateRelativeMass(PyObject * self, PyObject* args) {
-    
+    //return
 }
 
 static PyMethodDef calculationMethods[] = {
-	{"forc",calculateForce, METH_VARARGS, "Return the Force of interaction between 2 bodies - (a_x,a_y,a_radio,a_mass,b_x,b_y,b_radio,b_mass)"},
-	{"dist",calculateDist, METH_VARARGS, "Return the distance between 2 bodies - (a_x,a_y,a_radio,b_x,b_y,b_radio)"},
+	{"force",calculateForce, METH_VARARGS, "Return the Force of interaction between 2 bodies - (a_x,a_y,a_radius,a_mass,b_x,b_y,b_radius, b_mass)"},
+	{"dist",calculateDist, METH_VARARGS, "Return the distance between 2 bodies - (a_x,a_y,a_radius,b_x,b_y,b_radius)"},
 	//{"relativistic_mass",pyplanets_calculations_relativistic_mass,METH_VARARGS, "Return the relativistic mass of a body."},
 	//{"collision",pyplanets_calculations_collision,METH_VARARGS, "Return the new location and speed after the collision"},
 	{NULL, NULL}
 };
 
-void initCalculations(void){
-    PyImport_AddModule("calculations");
-    Py_InitModule("calculations", calculationMethods);
+static struct PyModuleDef calculations = 
+{
+    PyModuleDef_HEAD_INIT,
+    "calculations",
+    "",
+    -1,
+    calculationMethods
+};
+
+PyMODINIT_FUNC PyInit_calculations(void)
+{
+    return PyModule_Create(&calculations);
 }
--- main.py
+++ main.py
@@ -11,59 +11,78 @@ class Body:
         self.screen = screen
         self.x = startx
         self.y = starty
-        self.mass = mass
+        self.m = mass
         self.vx = float(velocityx)
         self.vy = float(velocityy)
         if not diameter: 
-            self.size = (self.mass/pi*5)**(1/2.0)
+            self.size = (self.m/pi*5)**(1/2.0)
         else:
             self.size = diameter
         self.color = (int(255-random.random()*200),int(255-random.random()*200),int(255-random.random()*200))
 
     def render(self):
-        pygame.draw.circle(self.screen, self.color, [self.x, self.y], self.size)
+        pygame.draw.circle(self.screen, self.color, [int(self.x), int(self.y)], self.size)
 
 class App(object):
-
-    def __init__(self, width=600, height=600):
+    def __init__(self, width=900, height=900):
         pygame.init()
         pygame.display.set_caption("Press ESC to quit")
         self.width = width
         self.height = height
         self.screen = pygame.display.set_mode((self.width, self.height), pygame.DOUBLEBUF)
-        self.background = pygame.Surface(self.screen.get_size()).convert()
+        self.background = pygame.Surface(self.screen.get_size())
+        self.background.convert() 
         self.font = pygame.font.SysFont('mono', 20, bold=True)
         self.bodies = []
         # maintains information between bodies to ease calculations
         self.values = {}
 
-    def interact(self, bodyA, bodyB):
+    def interact(self, A, B):
         # calculate force, distance between A and B
+        dx = B.x - A.x
+        dy = B.y - A.y
+        force = calculations.force(dx, dy, A.m, A.size, B.m, B.size)
+        dist = calculations.dist(dx, dy, A.size, B.size)
         # Use those values to update acceleration, and distance btwn
+        accelerationA = force / (A.m * 10000000000)
+        accelerationB = force / (B.m * 10000000000)
+        compAx = dx / dist
+        compAy = dy / dist
+        compBx = -dx / dist
+        compBy = -dy / dist
+        A.vx += accelerationA * compAx
+        A.vy += accelerationA * compAy
+        B.vx += accelerationB * compBx
+        B.vy += accelerationB * compBy
 
-        # acceleration = force / mass
-        # (<x, y>)velocity = acceleration * (<x, y>)Distance/TotalDistance
-
-        # Save values[(A, B)] = (force, (<x, y>)distance, TotalDistance) 
-        pass
+        # A.x -= A.vx
+        # A.y -= A.vy
+        # B.x -= B.vx
+        # B.y -= B.vy
+        
         
     def move(self):
         # call interact on all bodies
-        self.values = {}
-        for cA in self.bodies:
-            for cB in self.bodies:
-                if cA != cB:
-                    self.interact(cA,cB)
-        # update positions of all bodies
-        for obj in self.bodies:
-            obj.x += obj.vx
-            obj.y += obj.vy
+        numBodies = range(len(self.bodies))
+        for i in numBodies:
+            for j in numBodies[i+1:]:
+                    #print(i, self.bodies[i])
+                    self.interact(self.bodies[i], self.bodies[j])
+        for body in self.bodies:
+            body.x += body.vx
+            body.y += body.vy
 
     def draw(self):
         # Call each body's render function here
+        for body in self.bodies:
+            body.render()
         pass
 
+
     def run(self):
+        self.bodies.append(Body(self.screen, 450, 450, 0, 0, 10000, 5))
+        self.bodies.append(Body(self.screen, 300, 300, 0.05, 0, 100, 2))
+        
         running = True
         while running:
             for event in pygame.event.get():
@@ -72,14 +91,16 @@ class App(object):
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         running = False
-
-
+            
             pygame.display.flip()
             self.screen.blit(self.background, (0, 0))
+            #print(self.bodies[0].x)
+            self.move()
+            self.draw()
         pygame.quit()
 
 
 if __name__ == '__main__':
 
     # call with width of window
-    App(640, 400).run()
+    App(900, 900).run()
